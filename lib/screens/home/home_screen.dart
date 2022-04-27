359b0d6134b9626fa53fa6599ce0d8a3e90e831d
Mimoriam <mimoriam420@gmail.com>
2022-04-27 11:01:27 +0500

    Commit #2 - Some progress on the UI (TabBar)

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -21,14 +21,83 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin<HomeScreen> {
+  @override
+  bool get wantKeepAlive => true;
+
+  late TabController _controller;
+  bool indexIsNotOne = true;
+
+  @override
+  void initState() {
+    _controller = TabController(length: 5, vsync: this);
+    _controller.animateTo(1);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size _size = MediaQuery.of(context).size;
+    super.build(context);
+    // Size _size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: ResponsiveHelper(
-          mobile: Container(),
+          mobile: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  title: const Text('Tabs Demo'),
+                  pinned: true,
+                  floating: true,
+                  snap: false,
+                  forceElevated: innerBoxIsScrolled,
+                  bottom: TabBar(
+                    controller: _controller,
+                    isScrollable: true,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    labelPadding: const EdgeInsets.symmetric(horizontal: 14),
+                    onTap: (int index) {},
+                    tabs: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Tab(
+                          icon: Icon(Icons.camera_alt),
+                        ),
+                      ),
+                      Tab(text: "CHATS"),
+                      Tab(text: "GROUPS"),
+                      Tab(text: "STATUS"),
+                      Tab(text: "CALLS"),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(
+              controller: _controller,
+              children: [
+                SizedBox.expand(
+                  child: GestureDetector(
+                    onTap: () {
+                      _controller.animateTo(1);
+                    },
+                    child: Icon(Icons.directions_car, size: 350,),
+                  ),
+                ),
+                Icon(Icons.directions_transit),
+                Icon(Icons.directions_bike),
+                Icon(Icons.directions_car),
+                Icon(Icons.directions_transit),
+              ],
+            ),
+          ),
           tablet: Container(),
           desktop: Container(),
         ),
