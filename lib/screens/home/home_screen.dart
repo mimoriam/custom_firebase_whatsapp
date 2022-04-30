9cdf6e66d379941854ccd734e7c413fa57751b36
Mimoriam <mimoriam420@gmail.com>
2022-04-30 21:42:09 +0500

    Commit #16 - Big refactor from ResponsiveHelper to responsive_builder package. Now this app supports LandScape and Portrait transitions as well as on-demand UI rendering (previously, it was building widgets for mobile, tablet and desktop all at once; which imo was a pretty dumb decision on my part).

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -7,11 +7,11 @@ import 'package:custom_firebase_whatsapp/constants/constants.dart';
 
 /// Widgets:
 import 'package:custom_firebase_whatsapp/screens/home/widgets/camera_tab.dart';
-import 'package:custom_firebase_whatsapp/screens/home/widgets/chat_tab/chat_tab.dart';
-import 'package:custom_firebase_whatsapp/screens/home/widgets/group_tab/groups_tab.dart';
-import 'package:custom_firebase_whatsapp/screens/home/widgets/status_tab/status_tab.dart';
-import 'package:custom_firebase_whatsapp/screens/home/widgets/speed_dial.dart';
-import 'package:custom_firebase_whatsapp/screens/home/widgets/call_tab/call_tab.dart';
+import 'package:custom_firebase_whatsapp/screens/home/widgets/chat_tab.dart';
+import 'package:custom_firebase_whatsapp/screens/home/widgets/groups_tab.dart';
+import 'package:custom_firebase_whatsapp/screens/home/widgets/status_tab.dart';
+import 'package:custom_firebase_whatsapp/screens/home/speed_dial.dart';
+import 'package:custom_firebase_whatsapp/screens/home/widgets/call_tab.dart';
 import 'package:flutter/services.dart';
 
 /// Services:
@@ -21,7 +21,7 @@ import 'package:provider/provider.dart';
 import '../../state/theme_state.dart';
 
 /// Utils/Helpers:
-import '../../utils/responsive_helper.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 /// Entry Point:
 class HomeScreen extends StatefulWidget {
@@ -65,97 +65,109 @@ class _HomeScreenState extends State<HomeScreen>
     // Size _size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: ResponsiveHelper(
-          mobile: NestedScrollView(
-            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                  actionsIconTheme: const IconThemeData(
-                    color: Color(kAppBarIconsOrLabelColor),
-                  ),
-                  backgroundColor: const Color(kAppBarColor),
-                  title: const Text(
-                    'WhatsApp',
-                    style: TextStyle(color: Color(kAppBarIconsOrLabelColor)),
-                  ),
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: GestureDetector(
-                        onTap: () {
-                          Provider.of<ThemeStateProvider>(context, listen: false).toggleTheme();
-                        },
-                        child: Provider.of<ThemeStateProvider>(context).darkTheme
-                            ? const Icon(Icons.dark_mode)
-                            : const Icon(Icons.light_mode),
+        child: ScreenTypeLayout.builder(
+          mobile: (BuildContext context) {
+            return OrientationLayoutBuilder(
+              // Force a screen to stay in portrait/landscape. Overrides the OrientationLayoutBuilder
+              // mode: OrientationLayoutBuilderMode.portrait,
+              portrait: (context) => NestedScrollView(
+                headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    SliverAppBar(
+                      actionsIconTheme: const IconThemeData(
+                        color: Color(kAppBarIconsOrLabelColor),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 16),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: const Icon(Icons.search),
+                      backgroundColor: const Color(kAppBarColor),
+                      title: const Text(
+                        'WhatsApp',
+                        style: TextStyle(color: Color(kAppBarIconsOrLabelColor)),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 14),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: const Icon(Icons.more_vert),
-                      ),
-                    ),
-                  ],
-                  pinned: true,
-                  floating: true,
-                  snap: false,
-                  forceElevated: innerBoxIsScrolled,
-                  bottom: TabBar(
-                    controller: _controller,
-                    isScrollable: true,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    labelPadding: const EdgeInsets.symmetric(horizontal: 14),
-                    labelColor: const Color(kAppBarIndicatorColor),
-                    unselectedLabelColor: const Color(kAppBarIconsOrLabelColor),
-                    indicatorColor: const Color(kAppBarIndicatorColor),
-                    onTap: (int index) {},
-                    tabs: [
-                      Container(
-                        alignment: Alignment.center,
-                        child: const Tab(
-                          icon: Icon(Icons.camera_alt),
+                      actions: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: GestureDetector(
+                            onTap: () {
+                              Provider.of<ThemeStateProvider>(context, listen: false).toggleTheme();
+                            },
+                            child: Provider.of<ThemeStateProvider>(context).darkTheme
+                                ? const Icon(Icons.dark_mode)
+                                : const Icon(Icons.light_mode),
+                          ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const Icon(Icons.search),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 14),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const Icon(Icons.more_vert),
+                          ),
+                        ),
+                      ],
+                      pinned: true,
+                      floating: true,
+                      snap: false,
+                      forceElevated: innerBoxIsScrolled,
+                      bottom: TabBar(
+                        controller: _controller,
+                        isScrollable: true,
+                        indicatorSize: TabBarIndicatorSize.label,
+                        labelPadding: const EdgeInsets.symmetric(horizontal: 14),
+                        labelColor: const Color(kAppBarIndicatorColor),
+                        unselectedLabelColor: const Color(kAppBarIconsOrLabelColor),
+                        indicatorColor: const Color(kAppBarIndicatorColor),
+                        onTap: (int index) {},
+                        tabs: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: const Tab(
+                              icon: Icon(Icons.camera_alt),
+                            ),
+                          ),
+                          Tab(text: "CHATS"),
+                          Tab(text: "GROUPS"),
+                          Tab(text: "STATUS"),
+                          Tab(text: "CALLS"),
+                        ],
                       ),
-                      Tab(text: "CHATS"),
-                      Tab(text: "GROUPS"),
-                      Tab(text: "STATUS"),
-                      Tab(text: "CALLS"),
-                    ],
-                  ),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  controller: _controller,
+                  children: [
+                    CameraTab(controller: _controller),
+                    ChatTab(
+                      imageUrl: 'https://picsum.photos/id/237/200/300',
+                      itemCount: 5,
+                    ),
+                    GroupTab(
+                      imageUrl: 'https://picsum.photos/id/237/200/300',
+                      itemCount: 5,
+                    ),
+                    StatusTab(
+                      userProfileImage: 'https://picsum.photos/id/237/200/300',
+                      itemCount: 5,
+                    ),
+                    CallTab(),
+                  ],
                 ),
-              ];
-            },
-            body: TabBarView(
-              controller: _controller,
-              children: [
-                CameraTab(controller: _controller),
-                ChatTab(
-                  imageUrl: 'https://picsum.photos/id/237/200/300',
-                  itemCount: 5,
-                ),
-                GroupTab(
-                  imageUrl: 'https://picsum.photos/id/237/200/300',
-                  itemCount: 5,
-                ),
-                StatusTab(
-                  userProfileImage: 'https://picsum.photos/id/237/200/300',
-                  itemCount: 5,
-                ),
-                CallTab(),
-              ],
-            ),
-          ),
-          tablet: Container(),
-          desktop: Container(),
+              ),
+              landscape: (context) => Container(),
+            );
+          },
+          tablet: (BuildContext context) {
+            return OrientationLayoutBuilder(
+              portrait: (context) => Container(),
+              landscape: (context) => Container(),
+            );
+          },
+          desktop: (BuildContext context) => Container(),
         ),
       ),
       floatingActionButton: SpeedDialWidget(isDialOpen: isDialOpen),
--- lib/screens/home/speed_dial.dart
+++ lib/screens/home/speed_dial.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../constants/constants.dart';
+import '../../constants/constants.dart';
 
 /// Models:
 
